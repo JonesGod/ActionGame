14a3a814e62a490509711ca383d9347194c10d19
05PAI <95420855+05PAI@users.noreply.github.com>
2022-01-14 15:43:14 +0800

    修改攝影機碰到障礙物的反應
    1.修改Ray的角度比攝影機要更接近地面。
    2.增設視角的最大值與最小值。
    3.調高攝影機的高度。

--- ActionGameProject/Assets/ActionGame/Character/Player/Scrip/FolowCamera.cs
+++ ActionGameProject/Assets/ActionGame/Character/Player/Scrip/FolowCamera.cs
@@ -8,14 +8,15 @@ public class FolowCamera : MonoBehaviour
     public float followDistance;
     public Vector2 minMaxFollowDistance;
     private float horizontalAngle;
-    private float verticalAngle;    
+    private float verticalAngle;
     [HideInInspector]public Vector3 horizontalVector;
     [HideInInspector]public Vector3 cameraRight;
     public LayerMask checkHitLayerMask;
 
+    Vector3 cameraPosition;
     void Start()
     {
-        horizontalVector = lookTarget.transform.forward;        
+        horizontalVector = lookTarget.transform.forward;
     }
 
     void Update()
@@ -28,28 +29,31 @@ public class FolowCamera : MonoBehaviour
         float xAngle = Input.GetAxis("Mouse X");
         float yAngle = Input.GetAxis("Mouse Y");
         horizontalAngle = xAngle;
-        verticalAngle += yAngle;        
+        verticalAngle += yAngle;
+        
         //Debug.Log("horizontalAngle"+horizontalAngle); 
-        //Debug.Log("verticalAngle"+verticalAngle); 
-
+        //Debug.Log("verticalAngle"+verticalAngle);
+        if (verticalAngle > 20.0f)
+            verticalAngle=20.0f;
+        if (verticalAngle < -60.0f)
+            verticalAngle = -60.0f;
         //vector = Quaternion.AngleAxis(角度, 旋轉軸向量) * 欲旋轉向量;
         horizontalVector = Quaternion.AngleAxis(horizontalAngle, Vector3.up) * horizontalVector;
         horizontalVector.Normalize();
+        
         cameraRight = Vector3.Cross(Vector3.up, horizontalVector);
         Vector3 cameraForward = Quaternion.AngleAxis(verticalAngle, -cameraRight) * horizontalVector;
         cameraForward.Normalize();
-        Vector3 cameraPosition = lookTarget.position;
-
+        
         //牆壁檢測
-        if(Physics.Raycast(lookTarget.position, -cameraForward, out RaycastHit rh, followDistance, checkHitLayerMask))
+        if (Physics.Raycast(lookTarget.position+new Vector3(0.0f,1.0f,0.0f), -cameraForward , out RaycastHit rh, followDistance+1.0f, checkHitLayerMask))
         {
-            Vector3 hitRayDir = rh.point - lookTarget.position;
+            Vector3 hitRayDir = rh.point - lookTarget.position ;
             float hitRayLength = hitRayDir.magnitude;
             //Debug.Log("hitLength:" + hitRayLength);
+            Vector3 newCameraPosition = rh.point+ cameraForward*1.0f+ new Vector3(0.0f, 0.5f, 0.0f);//固定住攝影機的位置(不要再後退了)                        
 
-            Vector3 newCameraPosition = rh.point + cameraForward * 0.3f;//固定住攝影機的位置(不要再後退了)     
-
-            if(hitRayLength < minMaxFollowDistance.x)
+            if (hitRayLength < minMaxFollowDistance.x)
             {
                 float upDistance = minMaxFollowDistance.x - hitRayLength;
                 cameraPosition = newCameraPosition + Vector3.up * upDistance;
@@ -58,12 +62,14 @@ public class FolowCamera : MonoBehaviour
             {
                 cameraPosition = newCameraPosition;
             }
-            cameraForward = lookTarget.position - cameraPosition;
+            cameraForward = lookTarget.position + new Vector3(0.0f, 1.5f, 0.0f) - cameraPosition;
         }
         else
         {
-            cameraPosition = lookTarget.position - (cameraForward * followDistance);
+            cameraPosition = lookTarget.position+ new Vector3(0.0f, 1.5f, 0.0f) 
+                            - (cameraForward * followDistance);
         }
+        
         transform.forward = cameraForward;
         transform.position = cameraPosition;
     }
@@ -73,6 +79,6 @@ public class FolowCamera : MonoBehaviour
         Gizmos.color = new Color(1.0f, 0.0f, 0.0f);
         Gizmos.DrawLine(cameraRight, cameraRight * 3); 
         Gizmos.color = new Color(0.0f, 1.0f, 0.0f);    
-        Gizmos.DrawLine(horizontalVector, horizontalVector * 3);  
+        Gizmos.DrawLine(horizontalVector, horizontalVector * 3);    
     }
 }
