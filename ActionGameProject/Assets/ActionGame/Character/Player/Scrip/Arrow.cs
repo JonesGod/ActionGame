5e84dd385ca3364f90a3c465b1016af03d40d982
05PAI <95420855+05PAI@users.noreply.github.com>
2022-02-13 09:22:13 +0800

    Arrow腳本修改
    1.提高箭矢飛行速度
    2.降低箭矢墜落速度
    3.新增箭矢存活時間

--- ActionGameProject/Assets/ActionGame/Character/Player/Scrip/Arrow.cs
+++ ActionGameProject/Assets/ActionGame/Character/Player/Scrip/Arrow.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public float arrowSpeed = 30.0f;//箭矢飛行速度
-    public float gravity ;//重力
-
-    private float fallSpeed;//墜落速度
+    private float arrowSpeed = 40.0f;//箭矢飛行速度
+    private float gravity=3f ;//重力
+    private float liveTime=0.0f;//存在時間
+    private float fallSpeed=0.0f;//墜落速度
   
     // Start is called before the first frame update
     private void Awake()
@@ -16,8 +16,7 @@ public class Arrow : MonoBehaviour
     }
     void Start()
     {
-        gravity = 6.0f;
-        fallSpeed = 0.0f;
+
     }
     // Update is called once per frame
     void Update()
@@ -25,9 +24,23 @@ public class Arrow : MonoBehaviour
         transform.position += transform.forward * arrowSpeed * Time.deltaTime;//箭矢往前飛行速度
         fallSpeed += gravity*Time.deltaTime;//重力計算
         transform.position-= fallSpeed * Vector3.up*Time.deltaTime;//箭矢墜落速度
+
+        liveTime += Time.deltaTime;//計算存在時間
+        if(liveTime>=10f)//存在超過10秒時消失
+        {
+            ArrowDestory();
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
+        ArrowDestory();
+    }
+    /// <summary>
+    /// 箭矢消失
+    /// </summary>
+    void ArrowDestory()
+    {
+        liveTime = 0.0f;//初始化存在時間
         fallSpeed = 0.0f;//初始化箭矢的墜落速度
         gameObject.SetActive(false);
     }
