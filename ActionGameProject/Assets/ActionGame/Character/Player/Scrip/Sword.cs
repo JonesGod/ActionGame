ce484a17f4ed7321d7d300da5a4b2019e9593d8b
05PAI <95420855+05PAI@users.noreply.github.com>
2022-02-24 20:31:07 +0800

    Sword小修

--- ActionGameProject/Assets/ActionGame/Character/Player/Scrip/Sword.cs
+++ ActionGameProject/Assets/ActionGame/Character/Player/Scrip/Sword.cs
@@ -44,7 +44,7 @@ public class Sword : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (battleRunIsNext )
+        if (battleRunIsNext || attackState)
         {
             if(sword.gameObject.activeSelf==false)
                 SwordOn();
