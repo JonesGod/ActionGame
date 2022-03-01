0ef4543cf2a94d11018c980d6fe818591d4abd0e
JonesGod <qsc60811@gmail.com>
2022-03-02 04:14:38 +0800

    增加主選單和讀取進度
    1.增加主選單和載入進度(還差主選單圖片還沒放，拍一張場景的圖拿來用就好)

---
+++ ActionGameProject/Assets/ActionGame/UI/MainMenu/StartLoadingGame.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartLoadingGame : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text loadText;
+    
+    public void LoadLevel(string sceneName)
+    {
+        loadingScreen.SetActive(true);
+        StartCoroutine(DisplayLoadingScreen(sceneName));
+    }
+    IEnumerator DisplayLoadingScreen (string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);        
+
+        while (!operation.isDone)
+        {   
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            loadText.text = (progress * 100.0f) + "%";
+
+            slider.value = progress;
+            yield return null;
+        }
+    }
+}
