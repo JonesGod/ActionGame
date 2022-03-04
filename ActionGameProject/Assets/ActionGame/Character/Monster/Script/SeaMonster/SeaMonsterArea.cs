89e85d42b267cb86b2b917679398b9c31d892394
JonesGod <qsc60811@gmail.com>
2022-03-04 16:59:35 +0800

    海怪登場動畫做一半

--- ActionGameProject/Assets/ActionGame/Character/Monster/Script/SeaMonster/SeaMonsterArea.cs
+++ ActionGameProject/Assets/ActionGame/Character/Monster/Script/SeaMonster/SeaMonsterArea.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class SeaMonsterArea : MonoBehaviour
 {
     public GameObject[] trapMonsters;
+    public PlayableDirector seaMonsterTimeline;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            for(int i = 0; i < trapMonsters.Length; i++)
-            {
-                trapMonsters[i].SetActive(true);;
-            }       
+            seaMonsterTimeline.Play();
+            // for(int i = 0; i < trapMonsters.Length; i++)
+            // {
+            //     trapMonsters[i].SetActive(true);;
+            // }       
             Destroy(this.gameObject);     
         }
     }
