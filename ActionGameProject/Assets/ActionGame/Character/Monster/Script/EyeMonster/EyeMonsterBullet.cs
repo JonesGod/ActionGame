f42f979bb603448c0aea14267bded5789aa06b90
05PAI <95420855+05PAI@users.noreply.github.com>
2022-03-04 16:21:41 +0800

    新增眼球怪射擊命中音效

--- ActionGameProject/Assets/ActionGame/Character/Monster/Script/EyeMonster/EyeMonsterBullet.cs
+++ ActionGameProject/Assets/ActionGame/Character/Monster/Script/EyeMonster/EyeMonsterBullet.cs
@@ -8,12 +8,15 @@ public class EyeMonsterBullet : MonoBehaviour
     EyeMonsterBulletPool bulletPool;
     public float speed;
     private float lifeTime;
-    public ParticleSystem hitEffect;     
+    public ParticleSystem hitEffect;
+
+    public GameObject hitAudio;
     void OnEnable()
     {
         player = GameManager.Instance.GetPlayer().GetComponent<PlayerControl>();
         bulletPool = GameObject.Find("EyeMonsterBulletPool").GetComponent<EyeMonsterBulletPool>();
         lifeTime = 0.0f;
+
     }
 
     void Update()
@@ -30,7 +33,7 @@ public class EyeMonsterBullet : MonoBehaviour
         Debug.Log(other.name);
         if(other.transform.name == "NewLink")
         {
-            Debug.Log("眼球子彈打到玩家");            
+            Debug.Log("眼球子彈打到玩家");
             //呼叫玩家腳本的受傷function
             player.PlayerHurt(20);            
             bulletPool.Recovery(this.gameObject);
@@ -53,5 +56,6 @@ public class EyeMonsterBullet : MonoBehaviour
     public void PlayParticleSystem(ParticleSystem particle, Vector3 hitPosition)
     {
         Instantiate(particle, hitPosition, Quaternion.identity);
+        Instantiate(hitAudio);
     }
 }
