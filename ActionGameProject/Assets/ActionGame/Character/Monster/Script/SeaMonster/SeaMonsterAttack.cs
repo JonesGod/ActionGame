97933fdf7dacbcdea5903441b585a96528651e3d
05PAI <95420855+05PAI@users.noreply.github.com>
2022-03-05 10:26:40 +0800

    新增海怪出現與死亡咆嘯

--- ActionGameProject/Assets/ActionGame/Character/Monster/Script/SeaMonster/SeaMonsterAttack.cs
+++ ActionGameProject/Assets/ActionGame/Character/Monster/Script/SeaMonster/SeaMonsterAttack.cs
@@ -7,10 +7,20 @@ public class SeaMonsterAttack : MonoBehaviour
     public GameObject bulletSpawnPosition;
     public SeaMonsterBulletPool monsterBulletPool;
 
-    public GameObject shootAudio;
-    
+    public GameObject screamAudio;
+    private AudioSource screamSouce;
+
+    private void Start()
+    {
+        screamSouce = screamAudio.GetComponent<AudioSource>();
+    }
+
     public void ShootBullet()
     {
         monsterBulletPool.ReUse(bulletSpawnPosition.transform.position, bulletSpawnPosition.transform.rotation);       
+    }
+    void PlayScreamAudio()
+    {
+        screamSouce.Play();
     }
 }
