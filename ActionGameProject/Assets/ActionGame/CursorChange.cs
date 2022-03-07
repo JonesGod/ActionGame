b9b40c6727e4fefece0584fc28944df3a955fe0a
05PAI <95420855+05PAI@users.noreply.github.com>
2022-03-07 12:18:12 +0800

    調整主選單滑鼠圖案

---
+++ ActionGameProject/Assets/ActionGame/CursorChange.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour
+{
+    public Texture2D pointerImage;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Cursor.SetCursor(pointerImage, Vector2.zero, CursorMode.Auto);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
